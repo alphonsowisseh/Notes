58d06fbb1e0bf9369d3cc6294b37041b5582c474
Alphonso Wisseh <alphonso.wissehjr@gmail.com>
2019-04-24 18:35:47 -0400

    link notes

--- src/components/NoteList.js
+++ src/components/NoteList.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 export class NoteList extends Component {
     renderNotes(){
         const notes = Object.values(this.props.notes)
-        return notes.map((n) => <div><h2>{n.title}</h2></div>)
+        return notes.map((n) => <div><h2><Link to={`/notes/${n._id}`}>{n.title}</Link></h2></div>)
     }
   render() {
     return (
