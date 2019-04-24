1b62d682c9d83dd8d61bbb38aa24dfad00506c6e
Alphonso Wisseh <alphonso.wissehjr@gmail.com>
2019-04-24 18:13:20 -0400

    Move pages directory and fix IndexPage import

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import './App.css'
+import IndexPage from './pages/index'
 
 export class App extends Component {
   state = {
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-export class index extends Component {
+export class IndexPage extends Component {
   render() {
     return (
       <div>
