71efea64b324e13414771dd0acd170c663346654
Alphonso Wisseh <alphonso.wissehjr@gmail.com>
2019-04-24 18:16:31 -0400

    Testing

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react'
 
 export class IndexPage extends Component {
   render() {
+    const notes = Object.values(this.props.notes)
     return (
       <div>
         <h1>Notes</h1>
+        <h2>{notes[0].title}</h2>
       </div>
     )
   }
 }
 
-export default index
+export default IndexPage
