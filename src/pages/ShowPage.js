3088a925ddcef827cdbf7bcb29be0de3770e8bdd
Alphonso Wisseh <alphonso.wissehjr@gmail.com>
2019-04-24 19:53:11 -0400

    Update ShowPage.js

--- src/pages/ShowPage.js
+++ src/pages/ShowPage.js
@@ -3,6 +3,11 @@ import React, { Component } from 'react'
 export class ShowPage extends Component {
   render() {
       const { note } = this.props
+
+      if(!note){
+          return null
+      }
+      
     return (
       <div>
         <h1>{ note.title }</h1>
