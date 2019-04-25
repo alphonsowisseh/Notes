6ae3d9e6399edfc646c112c91c8aed38e0916ce9
Alphonso Wisseh <alphonso.wissehjr@gmail.com>
2019-04-24 20:31:26 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,35 @@
-import React, { Component } from 'react'
-import NoteList from '../components/NoteList'
+import React from 'react';
+import NoteList from '../components/NoteList';
 
-export class IndexPage extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Notes</h1>
-        <NoteList notes={this.props.notes} />
-      </div>
-    )
-  }
+import { Link } from 'react-router-dom';
+
+class IndexPage extends React.PureComponent {
+    constructor(props) {
+        super(props);
+
+        let notes = Object.values(props.notes)
+        notes.sort((a, b) => new Date(b.updatedAt) - new Date(a.updatedAt));
+
+        this.state = { notes };
+    }
+
+    render() {
+        let { notes } = this.state;
+
+        if (!notes.length) {
+            return (
+                <div className="app-intro">
+                    <h2>Welcome to Notes</h2>
+                    <p>You don't have any notes. <Link className="btn" to="/new">Get started!</Link></p>
+                </div>
+            );
+        }
+        return (
+            <div>
+                <h2>Notes</h2>
+                <NoteList notes={this.state.notes}/>
+            </div>
+        )
+    }
 }
-
-export default IndexPage
+export default IndexPage;
