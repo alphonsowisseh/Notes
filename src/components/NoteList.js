06c923aad7eaca41b44f91d5f50a031c22ab9a86
Alphonso Wisseh <alphonso.wissehjr@gmail.com>
2019-04-24 20:31:15 -0400

    Update NoteList.js

--- src/components/NoteList.js
+++ src/components/NoteList.js
@@ -1,18 +1,23 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import dayjs from 'dayjs';
 
-export class NoteList extends Component {
-    renderNotes(){
-        const notes = Object.values(this.props.notes)
-        return notes.map((n) => <div><h2><Link to={`/notes/${n._id}`}>{n.title}</Link></h2></div>)
-    }
-  render() {
-    return (
-      <div>
-        {this.renderNotes()}
-      </div>
-    )
-  }
+function renderDate(updatedAt) {
+    let d = dayjs(updatedAt);
+    return d.format("MMMM D YYYY, HH:mm");
 }
 
-export default NoteList
+const Note = ({ note: { _id, title, body, updatedAt } }) => (
+    <div className="notes-list-item">
+        <h2><Link to={`/notes/${_id}`}>{ title }</Link></h2>
+        <span className="notes-list-date">
+            { renderDate(updatedAt) }
+        </span>
+    </div>
+);
+
+export default ({ notes }) => (
+    <div className="notes-list">
+        {notes.map(n => <Note note={n} key={`note-${n._id}`}/> )}
+    </div>
+);
