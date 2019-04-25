4e248eff62a85f860842b4b6ff2567b37c173aef
Alphonso Wisseh <alphonso.wissehjr@gmail.com>
2019-04-24 20:31:29 -0400

    Update ShowPage.js

--- src/pages/ShowPage.js
+++ src/pages/ShowPage.js
@@ -1,20 +1,40 @@
-import React, { Component } from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import marked from 'marked';
+import dayjs from 'dayjs';
 
-export class ShowPage extends Component {
-  render() {
-      const { note } = this.props
+class ShowPage extends React.PureComponent {
+    renderDate() {
+        let d = dayjs(this.props.note.updatedAt)
+        return d.format("MMMM D YYYY, HH:mm")
+    }
 
-      if(!note){
-          return null
-      }
-      
-    return (
-      <div>
-        <h1>{ note.title }</h1>
-        <p>{ note.body }</p>
-      </div>
-    )
-  }
+    componentWillMount() {
+        if (!this.props.note) {
+            this.props.history.replace(`/`);
+            return;
+        }
+    }
+
+    render() {
+        const { note } = this.props;
+
+        if (!note) {
+            return null;
+        }
+
+        return (
+            <div>
+                <h1>{ note.title }</h1>
+                <div className="note-created">
+                    {this.renderDate()}
+                    <Link className="note-edit" to={`/notes/${note._id}/edit`}>Edit</Link>
+                    <button className="btn" onClick={ (e) => this.props.onDelete(note._id) }>Delete</button>
+                </div>
+                <div className="note-body" dangerouslySetInnerHTML={ { __html: marked(note.body) } } />
+            </div>
+        )
+    }
 }
 
-export default ShowPage
+export default ShowPage;
